eaa78e0ee2241d50dbc0e4ca96c448dba159b44a
cwchange <cwchange@gmail.com>
2015-08-03 07:53:25 -0400

    Update question_1.py

--- num1/question_1.py
+++ num1/question_1.py
@@ -1,3 +1,8 @@
+"""
+1. Write a function that prints the numbers from 1 to 100. But for multiples of three print “Foo” instead of the number and for the multiples of five print “Bar”. For numbers which are multiples of both three and five print “FooBar”. Example output: 1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 FooBar …
+"""
+
+
 #Solution to question 1
 def out_print(num):
         temp=1
